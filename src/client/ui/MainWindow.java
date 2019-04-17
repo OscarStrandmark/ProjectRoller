504cca1d91972c38e3780e0cc5d2f088d13df884
Patrik Skuza <patrikskuza@gmail.com>
2019-04-17 14:15:42 +0200

    Update MainWindow.java
    Lade till Move-Ikon funktionen.

--- src/client/ui/MainWindow.java
+++ src/client/ui/MainWindow.java
@@ -2,16 +2,24 @@ package client.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -21,6 +29,7 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -28,6 +37,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import client.Controller;
@@ -37,7 +47,7 @@ public class MainWindow extends JFrame {
 	private Controller controller;
 
 	//Components used in main window.
-	private JScrollPane boardPanel;
+	private JPanel boardPanel;
 	private JTabbedPane sidePanel;
 	private JSplitPane contentPane;
 
@@ -56,6 +66,14 @@ public class MainWindow extends JFrame {
 	private JButton importBtnIconFileChooser;
 	private JButton importBtnIconImport;
 
+	private JTextField scaleIcon;
+
+	private String imagePath;
+
+	JLabel iconicon;
+
+	private int iconWidth = 150;
+	private int iconHeight = 150;
 
 	//Components for settings-panel
 
@@ -64,13 +82,14 @@ public class MainWindow extends JFrame {
 		this.controller = controller;
 		init();
 		repaint();
+		setVisible(true);
+//		IconMovement iconMovement = new IconMovement(getComponents());
 	}
 
 	private void init() {
 		setTitle("Project Roller");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		setVisible(true);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 
 		//Code below sets the board-view to always be 80% of the screen.
@@ -119,6 +138,7 @@ public class MainWindow extends JFrame {
 		JPanel importPanel = new JPanel(new GridLayout(3, 1));
 
 		JPanel importIconPane = new JPanel();
+		importIconPane.setLayout(new GridLayout(2,2));
 
 		JPanel importBGPane = new JPanel(new GridLayout(4,1)); //Panel for importing a background.
 		importBGPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1)); //Creates a border for the panel
@@ -142,15 +162,28 @@ public class MainWindow extends JFrame {
 
 		JPanel importSaveLoadSessionPane = new JPanel(); //For future implementation of importing saved session data.
 
-
 		importPanel.add(importIconPane);
 		importPanel.add(importBGPane);
 		importPanel.add(importSaveLoadSessionPane);
 
+		// ------- IMPORT ICON --------
+
+		scaleIcon = new JTextField();
+		JLabel setSizeIcon = new JLabel("Set size for Icon (1-15)", SwingConstants.CENTER);
+
+		Border borderImport = BorderFactory.createLineBorder(Color.black);
+
+		importBtnIconFileChooser = new JButton("Choose Image");
+		importBtnIconImport = new JButton("Import Icon");
+
+		importIconPane.add(importBtnIconFileChooser);
+		importIconPane.add(importBtnIconImport);
+		importIconPane.add(setSizeIcon);
+		importIconPane.add(scaleIcon);
+		importIconPane.setBorder(borderImport);
 
 		// ---------- SETTINGS ----------
 		JPanel settingsPanel = new JPanel();
-
 
 		/*
 		 * ==============================================================
@@ -159,7 +192,8 @@ public class MainWindow extends JFrame {
 		 */
 
 		contentPane = new JSplitPane();
-		boardPanel = new JScrollPane();
+		boardPanel = new JPanel();
+		boardPanel.setLayout(null);
 		sidePanel = new JTabbedPane();
 
 		sidePanel.addTab("Chat", chatPanel);
@@ -183,7 +217,85 @@ public class MainWindow extends JFrame {
 		ButtonListener listener = new ButtonListener();
 
 		importBtnBackgroundFileChooser.addActionListener(listener);
-		importBtnBackgoundImport      .addActionListener(listener);
+		importBtnBackgoundImport.addActionListener(listener);
+
+		importBtnIconFileChooser.addActionListener(e -> openFileChooser());
+		importBtnIconImport.addActionListener(e -> setIconImage());
+	}
+
+	/**
+	 * When the "Choose Image"-button is clicked, show a FileChooser from which
+	 * a user can choose an image for an icon
+	 */
+	public void openFileChooser() {
+		JFileChooser fileChooser = new JFileChooser();
+		imagePath = null;
+
+		//Filter for what files to show in FileChooser-window.
+		FileNameExtensionFilter imageFilter = new FileNameExtensionFilter(
+				"Image files", ImageIO.getReaderFileSuffixes());
+		fileChooser.setFileFilter(imageFilter);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+		int fileOk = fileChooser.showOpenDialog(null);
+		if(fileOk == JFileChooser.APPROVE_OPTION) {
+			imagePath = fileChooser.getSelectedFile().getPath();
+		}
+	}
+
+	/**
+	 * When the "Import icon"-button is clicked, rescale the image
+	 * and show it on the gameboard.
+	 */
+	public void setIconImage() {
+		getScaleInput();
+		ImageIcon icon = new ImageIcon(imagePath);
+		Image image = icon.getImage();
+		Image newIconImage = getScaledImage(image, iconWidth, iconHeight);
+		ImageIcon finalIcon = new ImageIcon(newIconImage);
+		iconicon = new JLabel(finalIcon);
+		iconicon.setBounds(0,0,iconWidth,iconHeight);
+		boardPanel.add(iconicon);
+		boardPanel.repaint();
+		iconicon.addMouseListener(new IconMovement());
+		iconicon.addMouseMotionListener(new IconMovement());
+	}
+
+	/**
+	 * Scales the image by input from user. If no input is made,
+	 * make the icon the standard size of 150 by 150 pixels.
+	 */
+	public void getScaleInput() {
+		if(scaleIcon.getText().length() == 0) {
+		} else if(Integer.parseInt(scaleIcon.getText()) < 0 || Integer.parseInt(scaleIcon.getText()) > 15 ) {
+			JOptionPane.showMessageDialog(null, "Enter a valid number between 0-15,"
+					+ " or leave the field empty for a standard size of 150 by 150 "
+					+ "pixels icon.");
+		}else if(Integer.parseInt(scaleIcon.getText()) >= 1 && Integer.parseInt(scaleIcon.getText()) <= 15 ) {
+			int amountOfScale = Integer.parseInt(scaleIcon.getText());
+			iconWidth = amountOfScale * 20;
+			iconHeight = amountOfScale * 20;
+		} else {
+			JOptionPane.showMessageDialog(null, "Enter a valid number between 0-15,"
+					+ " or leave the field empty for a standard size of 150 by 150 "
+					+ "pixels icon.");
+		}
+	}
+
+	/**
+	 * Scales the chosen image to a set size.
+	 * @param srcImg the image to be scaled
+	 * @param w the width of the image
+	 * @param h the height of the image
+	 * @return return the new scaled image
+	 */
+	private Image getScaledImage(Image srcImg, int w, int h){
+		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = resizedImg.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.drawImage(srcImg, 0, 0, w, h, null);
+		g2.dispose();
+		return resizedImg;
 	}
 
 	private void setBoardBackground(Image img) {
@@ -218,8 +330,60 @@ public class MainWindow extends JFrame {
 					e1.printStackTrace();
 				}
 				g.drawImage(img, 0, 0, boardPanel.getWidth(), boardPanel.getHeight(), null);
-
 			}
 		}
+	}
+
+	private class IconMovement implements MouseListener, MouseMotionListener {
+		private int x,y;
+
+//		public IconMovement(Component... pns) {
+//			for(Component iconicon : pns) {
+//				iconicon.addMouseListener(this);
+//				iconicon.addMouseMotionListener(this);
+//			}
+//		}
+
+		@Override
+		public void mouseDragged(MouseEvent event) {
+				event.getComponent().setLocation((event.getX() + event.getComponent().getX())-x, (event.getY() + event.getComponent().getX())-y);
+			}
+
+		@Override
+		public void mouseMoved(MouseEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent event) {
+				x = event.getX();
+				y = event.getY();
+			}
+
+		@Override
+		public void mouseReleased(MouseEvent event) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
 	}
 }
