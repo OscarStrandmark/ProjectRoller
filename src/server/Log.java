5ce10ae3f34cdf539d1df2907d2bba6ca9f53796
Squaryyy <45971625+Squaryyy@users.noreply.github.com>
2019-05-28 13:24:14 +0200

    Update Log.java
    Comments

--- src/server/Log.java
+++ src/server/Log.java
@@ -5,36 +5,47 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
+/**
+ * Class handles all the logging in the program. Every action that is to be logged
+ * is saved as a .txt file on the computer hosting the server.
+ * @author Patrik Skuza
+ *
+ */
 public class Log {
-	Logger logger;
-	private FileHandler fileHandler;
+	Logger logger; //The logger object for logging of actions/messages
+	private FileHandler fileHandler; //FileHandler for the logger
 
+	/**
+	 * Constructor that creates a .txt file where log-messages are stored and attaches 
+	 * a FileHandler to the logger object.
+	 * @param fileName Name of the logfile
+	 */
 	public Log(String fileName) {
-		String directory = "c:\\dev\\logs\\";
-		File dir = new File(directory);
-		if(!dir.mkdirs()) {
+		String directory = "c:\\dev\\logs\\"; //The directory for where the logfile is saved
+		File dir = new File(directory); //Directory "path" for the actual logfile.
+		if(!dir.mkdirs()) { //If directory does not exist, create it.
 			dir.getParentFile().mkdirs();
 		}
-		File logFile = new File(dir + fileName);
-		String theLogger = directory + fileName;
+		File logFile = new File(dir + fileName); //Directory and filename of the logger
+		String theLogger = directory + fileName; //Directory and filename of the logger in String-format
 		if(!logFile.exists()) {
 			System.out.println("logFile no exists.");
 		} else {
 			System.out.println("logFile exists.");
 		}
 		try {
-			fileHandler = new FileHandler(theLogger, true);
+			fileHandler = new FileHandler(theLogger, true); //Create a FileHandler for the logger and make it appendable.
 			System.out.println(theLogger + "<- test");
-		} catch (SecurityException e) {
+		} catch (SecurityException e) { //Catch security violation.
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (IOException e) { //Cacth I/O exception.
 			e.printStackTrace();
 		}
-		logger = Logger.getLogger(theLogger);
-		logger.addHandler(fileHandler);
-		logger.setUseParentHandlers(false);
-		CustomFormatter formatter = new CustomFormatter();
-		fileHandler.setFormatter(formatter);
+		logger = Logger.getLogger(theLogger); //Assign the created log-object.
+		logger.addHandler(fileHandler); //Add the created FileHandler for the logger.
+		logger.setUseParentHandlers(false); //Prevent logger from sending log-messages to its parent.
+		CustomFormatter formatter = new CustomFormatter(); //Create a new instant of the CustomFormatter.
+		fileHandler.setFormatter(formatter); //Set the CustomFormatter to the FileHandler.
 	}
 
 	public Log() {
@@ -44,6 +55,10 @@ public class Log {
 		logger.info(msg + System.lineSeparator());
 	}
 
+	/**
+	 * Method logs the acquired message in the log .txtfile
+	 * @param msg Message to be logged
+	 */
 	public void logMessage(String msg) {
 		logger.info(msg + System.lineSeparator());
 	}
